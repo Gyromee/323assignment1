3cb03289193e67e934b69e85ab9c4f97ead8d398
Mevin Chen <mevin.chen@csu.fullerton.edu>
2018-11-08 23:35:38 -0800

    updated

--- SyntaxAnalyzer.java
+++ SyntaxAnalyzer.java
@@ -4,24 +4,29 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class SyntaxAnalyzer {
 
-	private String filename;
+	private String firstFile;
+	private String secondFile;
 	private String line;
+	private String tokenLine;
 	private String[] splitLine = null;
+	private String[] splitTokenLine = null;
+	private ArrayList<String[]> lastTokenInLine = new ArrayList<String[]>();
 	private char[] charString;
 	private int lineNumber = 0;
 	private int invalidLineNumber = 1;
 	
-	public SyntaxAnalyzer(String filename)
-	{
-		this.filename=filename;
+	public SyntaxAnalyzer(String firstFile, String secondFile){
+		this.firstFile = firstFile;
+		this.secondFile = secondFile;
 	}
 	
 	public void start() throws FileNotFoundException, IOException {
 		
-		try (BufferedReader br = new BufferedReader(new FileReader(filename))){
+		try (BufferedReader br = new BufferedReader(new FileReader(secondFile))){
 	    	BufferedWriter wr = new BufferedWriter(new FileWriter("syntaxResult.txt"));
 	    	
 	    	//Checks if there are any invalid tokens in our text file
@@ -34,25 +39,46 @@ public class SyntaxAnalyzer {
 	    		}
 	    		invalidLineNumber++;
 	    	 }
-	    	 BufferedReader rr = new BufferedReader(new FileReader(filename));	
-	    	 while((line = rr.readLine())!= null) {
-	         	//Trim whitespaces from line and store in string array
+	    	 //File with lexemes and tokens
+	    	 BufferedReader tokenFile = new BufferedReader(new FileReader(secondFile));	
+	    	 //File with the original file without token and lexemes separated
+	    	 BufferedReader oldFile = new BufferedReader(new FileReader(firstFile));
+	    	 //splitTokenLine contains the array that has both token and lexeme stored in the current line
+	    	 if(tokenFile.readLine() != null)
+	    		 //This skips the first line of the token file that says "Token      Lexeme"
+	    		 tokenLine = tokenFile.readLine();
+	    	 //while the original file is not the end of the text
+	    	 while((line = oldFile.readLine())!= null) {
+	         	//Trim whitespaces from Original Text File, line and store in string array
 	    		 splitLine = line.trim().split("\\s+");
-	    		 if(lineNumber == 0)
-	    		 {
-	    			 lineNumber++;
-	    			 continue;
-	    		 }
-	    		 if(lineNumber > 0) {
-		    		System.out.println("Token: " + splitLine[0] + "                  Lexeme: " + splitLine[1]);
-		    		//functionStatement(splitLine[0], splitLine[1]);
-		    		 
-		    		 
-	    		 }
+	    		 //trim whitespaces for token file
+	    		 if(tokenFile.readLine() != null) {
+	    			 splitTokenLine = tokenLine.trim().split("\\s+");
+	    			 }
+	    		 
+	    		 //For loop to have all the lexemes into an array list. 
+	    		 for(int i = 0; i < splitLine.length; i++) {
+	    			 //Checks if the the token equals the last character in our orignal text string
+	    			 if(splitTokenLine[1].equals(splitLine[splitLine.length - 1])) {
+	    				 break;
+	    			 }
+	    			 //else keep adding all the tokens into an array list
+	    			 else
+	    				 lastTokenInLine.add(new String[] {splitTokenLine[1]});
+	    			 
+	    			 
+	    			 if(tokenFile.readLine() != null) {
+	    				//Read in the next line and store it into splitTokenLine
+	    	    		 splitTokenLine = tokenLine.trim().split("\\s+");
+	    	    		 System.out.println("Token" + splitTokenLine[0] + " Lexeme " + splitTokenLine[1] );
+	    			 }
 
-	    		 //for(int lineNumber = )
- 				
-             
+	    		 }
+	    		 
+//	    		 for (String[] row : lastTokenInLine) {
+//	    	            System.out.println(row[0]);
+//	    	        }
+
 	      
 	    	 }
 	    	
